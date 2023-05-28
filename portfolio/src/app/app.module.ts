e2f9268a76b43f8351c15a22e2023bea86e406de
FabianCaspers <fabian.c1308@icloud.com>
2023-05-28 21:02:12 +0200

    finish education and technologies, added projects

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { StartPageComponent } from './components/start-page/start-page.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { WorkComponent } from './components/work/work.component';
+import { EducationComponent } from './components/education/education.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartPageComponent,
     OverviewComponent,
-    WorkComponent
+    WorkComponent,
+    EducationComponent,
+    TechnologiesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,

8edf3ee4f940c331ebe5b5c6c537fa55bc318f16
FabianCaspers <fabian.c1308@icloud.com>
2023-05-30 21:25:03 +0200

    added animation

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var require: any;
+const AOS = require('aos');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  ngOnInit(): void {
+    AOS.init({
+      // Optionally pass in options
+    });
+  }
 }
+
